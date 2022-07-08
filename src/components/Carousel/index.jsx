53fb1816264c89404b811dabb93aca4909ccb7e0
gloryFire <nvvquang1504@gmail.com>
2022-07-08 22:01:21 +0700

    update HoverSideBar Component

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -4,19 +4,23 @@ import {Box} from '@mui/material';
 import 'swiper/css';
 import "swiper/css/navigation";
 import {Navigation} from "swiper";
-
+import imgCarousel from '../../assets/sli-3090.png'
 const Carousel = () => {
    return (
       <Swiper navigation={true} modules={[Navigation]} className="my-swiper">
-         <SwiperSlide>Slide 1</SwiperSlide>
-         <SwiperSlide>Slide 2</SwiperSlide>
-         <SwiperSlide>Slide 3</SwiperSlide>
-         <SwiperSlide>Slide 4</SwiperSlide>
-         <SwiperSlide>Slide 5</SwiperSlide>
-         <SwiperSlide>Slide 6</SwiperSlide>
-         <SwiperSlide>Slide 7</SwiperSlide>
-         <SwiperSlide>Slide 8</SwiperSlide>
-         <SwiperSlide>Slide 9</SwiperSlide>
+         <SwiperSlide>
+            <Box>
+               <img src={imgCarousel} alt=""/>
+            </Box>
+         </SwiperSlide>
+         {/*<SwiperSlide>Slide 2</SwiperSlide>*/}
+         {/*<SwiperSlide>Slide 3</SwiperSlide>*/}
+         {/*<SwiperSlide>Slide 4</SwiperSlide>*/}
+         {/*<SwiperSlide>Slide 5</SwiperSlide>*/}
+         {/*<SwiperSlide>Slide 6</SwiperSlide>*/}
+         {/*<SwiperSlide>Slide 7</SwiperSlide>*/}
+         {/*<SwiperSlide>Slide 8</SwiperSlide>*/}
+         {/*<SwiperSlide>Slide 9</SwiperSlide>*/}
       </Swiper>
    );
 };
