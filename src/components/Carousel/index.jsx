64222b59341744f1bb9136faba241fff94cb2f92
gloryFire <nvvquang1504@gmail.com>
2022-07-08 16:22:08 +0700

    update

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -7,19 +7,17 @@ import {Navigation} from "swiper";
 
 const Carousel = () => {
    return (
-      <Box className={'swiper-container'}>
-         <Swiper navigation={true} modules={[Navigation]} className="my-swiper">
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
-         </Swiper>
-      </Box>
+      <Swiper navigation={true} modules={[Navigation]} className="my-swiper">
+         <SwiperSlide>Slide 1</SwiperSlide>
+         <SwiperSlide>Slide 2</SwiperSlide>
+         <SwiperSlide>Slide 3</SwiperSlide>
+         <SwiperSlide>Slide 4</SwiperSlide>
+         <SwiperSlide>Slide 5</SwiperSlide>
+         <SwiperSlide>Slide 6</SwiperSlide>
+         <SwiperSlide>Slide 7</SwiperSlide>
+         <SwiperSlide>Slide 8</SwiperSlide>
+         <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
    );
 };
 
