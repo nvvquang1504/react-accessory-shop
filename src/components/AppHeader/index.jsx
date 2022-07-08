25c44e6fe5fd5df26a4151b22cd0d848594aa64a
gloryFire <nvvquang1504@gmail.com>
2022-07-08 08:16:01 +0700

    update

--- src/components/AppHeader/index.jsx
+++ src/components/AppHeader/index.jsx
@@ -1,16 +1,29 @@
 import './style.scss'
-import {Box} from '@mui/material';
-import TopBar from './TopBar'
-import MiddleBar from './MiddleBar'
-import NavBar from './NavBar'
+import {Box, Grid, IconButton} from '@mui/material';
+import lvlLogo from '../../assets/lvllogo.png';
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {styled} from "@mui/material/styles";
 
 const AppHeader = () => {
    
    return (
-      <Box className={'app-header'}>
-         <TopBar/>
-         <MiddleBar/>
-         <NavBar/>
+      <Box px={3} className={'app-header'}>
+         <Grid container>
+            <Grid item xs={2}>
+               <img src={lvlLogo} alt="logo"/>
+               <IconButton className={'app-header__hamburger'} disableRipple={true} aria-label="hamburger">
+                  <GiHamburgerMenu/>
+               </IconButton>
+            </Grid>
+            <Grid item xs={5}>
+               <div className={'search-bar'}>
+                  <input className={'items'} type="text" placeholder={'Bạn muốn tìm sản phẩm nào'}/>
+                  <button className={'items'}>Tìm kiếm</button>
+               </div>
+            </Grid>
+            <Grid item xs={3}></Grid>
+            <Grid item xs={2}></Grid>
+         </Grid>
       </Box>
    );
 };
