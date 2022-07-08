0c18600f25de276f0000a7e7d6fa0c5605cb909f
gloryFire <nvvquang1504@gmail.com>
2022-07-08 11:43:31 +0700

    update

--- src/components/AppHeader/index.jsx
+++ src/components/AppHeader/index.jsx
@@ -1,5 +1,5 @@
 import './style.scss';
-import {Box, Grid, IconButton, Badge, Stack} from '@mui/material';
+import {Box, Grid, IconButton, Badge, Stack, Container} from '@mui/material';
 import lvlLogo from '../../assets/lvllogo.png';
 import {GiHamburgerMenu} from 'react-icons/gi';
 import {BiPhoneCall} from 'react-icons/bi';
@@ -17,77 +17,112 @@ const AppHeader = () => {
    }, [])
    
    return (
-      <Box px={width < 1200 ? 9 : 10} className={'app-header'}>
-         {
-            width < 1200
-               ?
-               <Grid className={'app-header__mobile'} container>
-                  <Grid className={'mobile'} item container xs={12}>
-                     <Grid item xs={6}>
-                        <img src={lvlLogo} alt="logo"/>
-                     </Grid>
-                     <Grid item xs={6}></Grid>
-                  </Grid>
-                  <Grid item xs={12}>
-                     <IconButton className={'app-header__hamburger'} disableRipple={true} aria-label="hamburger">
-                        <GiHamburgerMenu/>
-                     </IconButton>
-                     <div className={'search-bar'}>
-                        <input className={'items'} type="text" placeholder={'Bạn muốn tìm sản phẩm nào'}/>
-                        <button className={'items'}>
-                           <AiOutlineSearch/>
-                        </button>
-                     </div>
-                  </Grid>
-               </Grid>
-               
-               : <Grid className={'app-header__pc'} container>
-                  <Grid item xs={2}>
-                     <img src={lvlLogo} alt="logo"/>
-                     <IconButton className={'app-header__hamburger'} disableRipple={true} aria-label="hamburger">
-                        <GiHamburgerMenu/>
-                     </IconButton>
-                  </Grid>
-                  <Grid item xs={4}>
-                     <div className={'search-bar'}>
-                        <input className={'items'} type="text" placeholder={'Bạn muốn tìm sản phẩm nào'}/>
-                        <button className={'items'}>Tìm kiếm</button>
-                     </div>
-                  </Grid>
-                  <Grid item xs={3}>
-                     <div>
-                        <BiPhoneCall/>
-                        <div className="contact">
-                           <p>Hot line</p>
-                           <b>1900 0243 (8:30 - 21:00)</b>
-                        </div>
-                     </div>
-                  </Grid>
-                  <Grid item xs={1}>
-                     <div>
-                        <Badge
+      <Box className={'app-header'}>
+         <Container>
+            {
+               width < 1200
+                  ?
+                  <Grid className={'app-header__mobile'} container>
+                     <Grid className={'mobile'} item container xs={12} spacing={2}>
+                        <Grid item xs={6}>
+                           <img src={lvlLogo} alt="logo"/>
+                        </Grid>
+                        <Grid item xs={6} height={'100%'} justifyContent={"space-between"}>
+                           <Stack spacing={2}>
+                              <div>
+                                 <BiPhoneCall/>
+                                 <div className="contact">
+                                    <p>Hot line</p>
+                                    <b>1900 0243 (8:30 - 21:00)</b>
+                                 </div>
+                              </div>
+                              <Stack direction={'row'} spacing={8}>
+                                 <div>
+                                    <Badge
+                                       
+                                       color="primary" badgeContent={9}
+                                       anchorOrigin={{
+                                          vertical: 'bottom',
+                                          horizontal: 'right',
+                                       }}
+                                    >
+                                       <BsBag className={'bs-bag'}/>
+                                    </Badge>
+                                 </div>
+                                 <div style={{cursor: 'pointer'}}>
+                                    <FaRegUser/>
+                                    <div className={'login-logout'}>
+                                       <b>Dang nhap</b>
+                                       <b>Dang ky</b>
+                                    </div>
+                                 </div>
+                              </Stack>
                            
-                           color="primary" badgeContent={9}
-                           anchorOrigin={{
-                              vertical: 'bottom',
-                              horizontal: 'right',
-                           }}
-                        >
-                           <BsBag className={'bs-bag'}/>
-                        </Badge>
-                     </div>
-                  </Grid>
-                  <Grid item xs={2}>
-                     <div style={{cursor: 'pointer'}}>
-                        <FaRegUser/>
-                        <div className={'login-logout'}>
-                           <b>Dang nhap</b>
-                           <b>Dang ky</b>
+                           </Stack>
+                        
+                        </Grid>
+                     </Grid>
+                     <Grid item xs={12}>
+                        <IconButton className={'app-header__hamburger'} disableRipple={true} aria-label="hamburger">
+                           <GiHamburgerMenu/>
+                        </IconButton>
+                        <div className={'search-bar'}>
+                           <input className={'items'} type="text" placeholder={'Bạn muốn tìm sản phẩm nào'}/>
+                           <button className={'items'}>
+                              <AiOutlineSearch/>
+                           </button>
                         </div>
-                     </div>
+                     </Grid>
                   </Grid>
-               </Grid>
-         }
+                  
+                  : <Grid className={'app-header__pc'} container>
+                     <Grid item xs={2}>
+                        <img src={lvlLogo} alt="logo"/>
+                        <IconButton className={'app-header__hamburger'} disableRipple={true} aria-label="hamburger">
+                           <GiHamburgerMenu/>
+                        </IconButton>
+                     </Grid>
+                     <Grid item xs={4}>
+                        <div className={'search-bar'}>
+                           <input className={'items'} type="text" placeholder={'Bạn muốn tìm sản phẩm nào'}/>
+                           <button className={'items'}>Tìm kiếm</button>
+                        </div>
+                     </Grid>
+                     <Grid item xs={3}>
+                        <div>
+                           <BiPhoneCall/>
+                           <div className="contact">
+                              <p>Hot line</p>
+                              <b>1900 0243 (8:30 - 21:00)</b>
+                           </div>
+                        </div>
+                     </Grid>
+                     <Grid item xs={1}>
+                        <div>
+                           <Badge
+                              
+                              color="primary" badgeContent={9}
+                              anchorOrigin={{
+                                 vertical: 'bottom',
+                                 horizontal: 'right',
+                              }}
+                           >
+                              <BsBag className={'bs-bag'}/>
+                           </Badge>
+                        </div>
+                     </Grid>
+                     <Grid item xs={2}>
+                        <div style={{cursor: 'pointer'}}>
+                           <FaRegUser/>
+                           <div className={'login-logout'}>
+                              <b>Dang nhap</b>
+                              <b>Dang ky</b>
+                           </div>
+                        </div>
+                     </Grid>
+                  </Grid>
+            }
+         </Container>
       </Box>
    );
 };
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,25 @@
+import './style.scss'
+import {Box, Container} from '@mui/material';
+
+const NavBar = () => {
+   return (
+      <Box bgcolor={'primary'}>
+         <Container>
+            <nav>
+               <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+               </ul>
+            </nav>
+         </Container>
+      
+      </Box>
+   );
+};
+
+export default NavBar;
