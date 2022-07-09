28fc60258238d4fe111dba343edaa4e495530d2f
gloryFire <nvvquang1504@gmail.com>
2022-07-09 10:45:56 +0700

    update

--- src/utils/myData.js
+++ src/utils/myData.js
@@ -22,18 +22,50 @@ export const sideBarData = [
          {
             title: {_id: 'mainboard-bomachchu', en: 'CPU', vi: 'Bộ xử lý'},
             items: [
-               {_id: 'i-pentium', title: 'Intel Pentium'},
-               {_id: 'i-celeron', title: 'Intel Celeron'},
-               {_id: 'i-corei3', title: 'Intel Core i3'},
-               {_id: 'i-corei5', title: 'Intel Core i5'},
-               {_id: 'i-corei7', title: 'Intel Core i7'},
-               {_id: 'i-corei9', title: 'Intel Core i9'},
-               {_id: 'i-xeon', title: 'Intel Xeon'},
-               {_id: 'amd-athlon', title: 'AMD Athlon'},
-               {_id: 'amd-ryzen3', title: 'AMD Ryzen 3'},
-               {_id: 'amd-ryzen5', title: 'AMD Ryzen 5'},
-               {_id: 'amd-ryzen7', title: 'AMD Ryzen 7'},
-               {_id: 'amd-ryzen9', title: 'AMD Ryzen 9'},
+               {_id: 'main-asus', title: 'Main ASUS'},
+               {_id: 'main-asrock', title: 'Main ASROCK'},
+               {_id: 'main-afox', title: 'Main AFOX'},
+               {_id: 'main-colorful', title: 'Main COLORFUL'},
+               {_id: 'main-gigabyte', title: 'Main GIGABYTE'},
+               {_id: 'main-msi', title: 'Main MSI'},
+               {_id: 'main-amd', title: 'Main AMD'},
+               {_id: 'main-dongb', title: 'Main dòng B '},
+               {_id: 'main-dongh', title: 'Main dòng H'},
+               {_id: 'main-dongz', title: 'Main dòng Z'},
+               {_id: 'main-hangkhac', title: 'Main hãng khác'},
+            ]
+         },
+         {
+            title: {_id: 'ram-bonhoram', en: 'RAM', vi: 'Bộ nhớ ram'},
+            items: [
+               {_id: 'ram-ddr3-pc', title: 'Ram DDR3 Pc'},
+               {_id: 'ram-ddr4-pc', title: 'Ram DDR4 Pc'},
+               {_id: 'ram-ddr5-pc', title: 'Ram DDR5 Pc'},
+               {_id: 'ram-ddr3-server', title: 'Ram DDR3 Server'},
+               {_id: 'ram-ddr4-server', title: 'Ram DDR4 Server'},
+               {_id: 'ram-4gb-8gb', title: 'Ram 4GB, 8GB'},
+               {_id: 'ram-16gb-32gb', title: 'Ram 16GB, 32GB'},
+               {_id: 'ram-kit', title: 'Ram Kit'},
+               {_id: 'ram-led', title: 'Ram Led'},
+            ]
+         },
+         //VGA
+         {
+            title: {_id: 'vga-cardmanhinh', en: 'VGA', vi: 'Card màn hình'},
+            items: [
+               {_id: 'vga-asus', title: 'VGA Asus'},
+               {_id: 'vga-colorful', title: 'VGA Colorful'},
+               {_id: 'vga-gigabyte', title: 'VGA Gigabyte'},
+               {_id: 'vga-msi', title: 'VGA MSI'},
+               {_id: 'vga-galax', title: 'VGA Galax'},
+               {_id: 'vga-palit', title: 'VGA Palit'},
+               {_id: 'vga-zotac', title: 'VGA Zotac'},
+               {_id: 'vga-amd', title: 'VGA AMD'},
+               {_id: 'vga-gf-gtx-10-series', title: 'VGA Geforce® 10 GTX'},
+               {_id: 'vga-gf-gtx-16-series', title: 'VGA Geforce® 16 GTX'},
+               {_id: 'vga-gf-gtx-20-series', title: 'VGA Geforce® 20 GTX'},
+               {_id: 'vga-gf-gtx-30-series', title: 'VGA Geforce® 30 GTX'},
+               {_id: 'vga-nvidia-quadro', title: 'VGA Nvidia Quadro'},
             ]
          }
       ]
